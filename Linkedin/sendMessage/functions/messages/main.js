49989fbfaee97addaf3272faf0d4005266d59e0a
sebastian-leandro <sebas.officciall@gmail.com>
2024-01-21 12:45:16 -0300

    .

--- Linkedin/sendMessage/functions/messages/main.js
+++ Linkedin/sendMessage/functions/messages/main.js
@@ -1,4 +1,5 @@
 import { messageSelectors } from '../../constants/variables.js'
+import { wait } from '../static/timers.js'
 
 let counter = 0
 
@@ -11,6 +12,7 @@ export async function * main (invitationsSet, messagesSet, page) {
           try {
             await search.type(invitation)
             yield { action: 'handleInput' }
+            await wait(2000, 1000)
             const warning = await page.$(messageSelectors.invitationNotAccept)
             if (warning === undefined || warning === null) {
               yield { action: 'handleWarning' }
@@ -19,6 +21,9 @@ export async function * main (invitationsSet, messagesSet, page) {
               yield { action: 'handleFinish', number: counter }
               yield { action: 'handleWait', user: invitation, number: counter }
               counter++
+            } else {
+              const newMessage = await page.$(messageSelectors.btnFilter)
+              if (newMessage) { await newMessage.click() }
             }
           } catch (err) { yield { action: 'handleError', error: err.message } }
         }
