35426676d41d74f32da36ff869b845a56d5552e8
sebastian-leandro <sebas.officciall@gmail.com>
2024-01-27 12:27:43 -0300

    update styles

--- Instagram/Follow/Follow/follow/api/server.ts
+++ Instagram/Follow/Follow/follow/api/server.ts
@@ -1,13 +1,20 @@
-import express from 'express'
+import express, { type Request, type Response } from 'express'
 
 const app = express()
 const PORT = 3000
+
+app.use(express.json()) // Middleware to parse incoming JSON data
 
 app.get('/api/data', (req, res) => {
   res.json({ name: 'John', age: 30 })
 })
 
-app.post('/api/data', (req, res) => {
+app.post('/api/data', (req: Request, res: Response) => {
+  // Parse incoming JSON data
+  const { username, password } = req.body
+  console.log(username, password)
+  // Perform any necessary logic with username and password
+  // For now, just respond with success
   res.json({ success: true })
 })
 
--- Instagram/Follow/Follow/follow/src/App.tsx
+++ Instagram/Follow/Follow/follow/src/App.tsx
@@ -1,6 +1,59 @@
+import { useState } from 'react'
+
+import classes from './App.module.scss'
+
 function App (): React.ReactNode {
+  const [username, setUsername] = useState<string>('')
+  const [password, setPassword] = useState<string>('')
+  const [success, setSuccess] = useState<boolean>(false)
+  const [error, setError] = useState<string | null>(null)
+
+  const handleUserChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setUsername(e.target.value)
+  }
+  const handlePassChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setPassword(e.target.value)
+  }
+  const handleLogin = async (): Promise<void> => {
+    if (username === '' || password === '') {
+      alert('Please fill in all fields')
+      return
+    }
+    try {
+      const response = await fetch('/api/data', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ username, password })
+      })
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`)
+      }
+
+      const data = await response.json()
+      setSuccess(true)
+      console.log(data)
+    } catch (error: any) {
+      setError(`There was an error ${error}`)
+      console.log(error)
+    }
+  }
+
   return (
     <>
+      <section className={classes.wrapper}>
+        <h1 className={classes.title}>Instagram Automation Tool</h1>
+        <div className={classes.container}>
+          <form>
+            <input type="text" placeholder="Username" className={classes.input} onChange={handleUserChange} />
+            <input type="password" placeholder="Password" className={classes.input} onChange={handlePassChange} />
+            <button type='button' className={classes.btn} onClick={handleLogin}>Login</button>
+          </form>
+        </div>
+        {success && <p className={classes.success}>Success</p>}
+        {error !== null && <p className={classes.error}>{error}</p>}
+      </section>
     </>
   )
 }
